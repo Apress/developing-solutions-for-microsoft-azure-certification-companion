8c5d2d0687ff6a0d53b449028b194917eaf9e1e9
Shaul Elson <116119311+shaulelson@users.noreply.github.com>
2023-05-17 12:21:16 -0400

    Add files via upload

---
+++ ch02/WorkingWithAzureCosmosDB/University.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithAzureCosmosDB
+{
+    public class University
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int YearFounded { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ ch02/WorkingWithAzureTableStorage/University.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithAzureTableStorage
+{
+    public class University : ITableEntity
+    {
+        public string RowKey { get; set; } = default!;
+        public string PartitionKey { get; set; } = default!;
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int YearFounded { get; set; }
+        public DateTimeOffset? Timestamp { get; set; } = default!;
+        public ETag ETag { get; set; } = default!;
+    }
+}
